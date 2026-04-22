f29b18367e90428843137d74551e432385d255d7
gwiazdan <norbert.gwiazda@icloud.com>
2026-04-22 19:21:21 +0200

    hotfix

--- prototype/src/visibility.py
+++ prototype/src/visibility.py
@@ -1,9 +1,7 @@
-def divide_screen(screen, coords, min_x, max_x, min_y, max_y): 
-    if (max_y-min_y)>2 && (max_x-min_x)>2:
-        divide_screen(screen, coords, min_x, max_x/2, min_y, max_y/2)
-        divide_screen(screen, coords, max_x/2+1, max_x, min_y, max_y/2)
-        divide_screen(screen, coords, max_x, max_x/2, max_y/2+1, max_y)
-        divide_screen(screen, coords, max_x/2+1, max_x, max_y/2+1, max_y)
+def divide_screen(screen, coords, min_x, max_x, min_y, max_y):
+    if (max_y - min_y) > 2 and (max_x - min_x) > 2:
+        divide_screen(screen, coords, min_x, max_x / 2, min_y, max_y / 2)
+        divide_screen(screen, coords, max_x / 2 + 1, max_x, min_y, max_y / 2)
+        divide_screen(screen, coords, max_x, max_x / 2, max_y / 2 + 1, max_y)
+        divide_screen(screen, coords, max_x / 2 + 1, max_x, max_y / 2 + 1, max_y)
     pass
-
-
